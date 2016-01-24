702af230693a3e049584d6a350848a3828a33a46
Nick Burrell <nicholas@burrellhome.com>
2016-01-24 15:27:14 -0800

    What is love?

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+    g++ ./main.cpp -o axis.out -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_features2d -lopencv_video
--- install.sh
+++ install.sh
@@ -7,13 +7,14 @@ sudo apt-get install build-essential checkinstall git cmake libfaac-dev libjack-
 sudo apt-get install libgstreamer0.10-0 libgstreamer0.10-dev gstreamer0.10-tools gstreamer0.10-plugins-base libgstreamer-plugins-base0.10-dev gstreamer0.10-plugins-good gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad gstreamer0.10-ffmpeg;
 sudo apt-get install libgtk2.0-0 libgtk2.0-dev;
 sudo apt-get install libjpeg8 libjpeg8-dev;
+sudo apt-get install unzip;
 cd ~;
 mkdir src;
 cd ~/src;
 sudo wget https://github.com/Itseez/opencv/archive/2.4.11.zip;
-sudo unzip ./2.4.11.zip;
+unzip ./2.4.11.zip;
 cd opencv-2.4.11;
 echo "Enter the valid options into CMake Gui, and press generate"
-sudo cmake-gui;
+sudo cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local
 make;
 sudo make install;
